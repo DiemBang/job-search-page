6a706cd4f68658366caaf9978056621b7cd85974
Diem Bang <diembang09@gmail.com>
2024-09-25 14:49:04 +0200

    :children_crossing: Add spinner

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,28 +2,42 @@ import { createBrowserRouter } from "react-router-dom";
 import { NotFound } from "./pages/NotFound";
 import { jobPageLoader } from "./loaders/jobPageLoader";
 import { searchPageLoader } from "./loaders/searchPageLoader";
+import { DigiLoaderSpinner } from "@digi/arbetsformedlingen-react";
+import { Suspense } from "react";
+import { LoaderSpinnerSize } from "@digi/arbetsformedlingen";
 
 export const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    errorElement: <NotFound></NotFound>,
+    children: [
+      {
         path: "/",
-        element: <Layout></Layout>,
-        errorElement: <NotFound></NotFound>,
-        children: [
-            {
-                path: "/",
-                element: <Home></Home>,
-                index: true
-            },
-            {
-                path: "/search",
-                element: <Search></Search>,
-                loader: searchPageLoader
-            },
-            {
-                path: "/job/:id",
-                element: <JobPage></JobPage>,
-                loader: jobPageLoader
+        element: <Home></Home>,
+        index: true,
+      },
+      {
+        path: "/search",
+        element: (
+          <Suspense
+            fallback={
+              <DigiLoaderSpinner
+                afSize={LoaderSpinnerSize.LARGE}
+                afText="Laddar"
+              ></DigiLoaderSpinner>
             }
-        ]
-    }
-])
+          >
+            <Search></Search>
+          </Suspense>
+        ),
+        loader: searchPageLoader,
+      },
+      {
+        path: "/job/:id",
+        element: <JobPage></JobPage>,
+        loader: jobPageLoader,
+      },
+    ],
+  },
+]);
