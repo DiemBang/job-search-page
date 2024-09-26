f7ec11d9a7195a9ab7781ea044b0029f6565bd63
armin-164 <arminsadr164@hotmail.com>
2024-09-27 00:33:33 +0200

    feat: add Home component
    Addd Home component which includes search input integration.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import { FormInputSearchVariation, FormInputType, LayoutBlockVariation } from '@digi/arbetsformedlingen';
+import { DigiFormInputSearch, DigiLayoutBlock } from '@digi/arbetsformedlingen-react';
+
+export const Home = () => {
+  return (
+    <>
+      <DigiLayoutBlock
+        afVariation={LayoutBlockVariation.PRIMARY}
+        className="home-main-block"
+      >
+        <section className="home-top-section"></section>
+        <section className="home-middle-section">
+          <DigiFormInputSearch
+            afLabel="Sök Här"
+            afVariation={FormInputSearchVariation.MEDIUM}
+            afType={FormInputType.SEARCH}
+            afButtonText="Knapptext"
+            className="home-search-container"
+          ></DigiFormInputSearch>
+        </section>
+        <section className="home-bottom-section"></section>
+      </DigiLayoutBlock>
+    </>
+  );
+};
