ec2890974471f86222e0cbb020eb25278e482d3a
RalfiSlask <nilssonnarki@gmail.com>
2024-09-26 06:41:17 +0200

    ✨ add utils function for converting a number to a swedish word

---
+++ src/utils/numberStringUtils.ts
@@ -0,0 +1,34 @@
+export const numberToWords = (number: number) => {
+  const units = [
+    '',
+    'ett',
+    'två',
+    'tre',
+    'fyra',
+    'fem',
+    'sex',
+    'sju',
+    'åtta',
+    'nio',
+  ];
+  const teens = [
+    'tio',
+    'elva',
+    'tolv',
+    'tretton',
+    'fjorton',
+    'femton',
+    'sexton',
+    'sjutton',
+    'arton',
+    'nitton',
+  ];
+
+  if (number <= 0) return '';
+  if (number <= 20) {
+    if (number < 10) return units[number];
+    if (number < 20) return teens[number - 10];
+  }
+
+  return 'Över 20 jobb';
+};
