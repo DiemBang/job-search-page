d65093e025d479bda15f673096a4e2d918207665
Diem Bang <diembang09@gmail.com>
2024-09-26 23:32:36 +0200

    :lipstick: Update wrapper width

--- src/components/styled/Wrappers.tsx
+++ src/components/styled/Wrappers.tsx
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 export const FilterWrapper = styled.div`
-    width: 100%;
+    width: 100vw;
     display: flex;
     flex-wrap: wrap;
     gap: 1rem;
 `;
 
 export const SearchResultWrapper = styled.div`
-    width: 100%;
+    width: 100vw;
     margin: 1em;
     padding: 1em;
     background-color: beige;
