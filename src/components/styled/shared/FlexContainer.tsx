950986ed273c5bf92a5122af819c905e298442b1
RalfiSlask <nilssonnarki@gmail.com>
2024-09-26 19:11:50 +0200

    ✨ upgrade styled flex-container

--- src/components/styled/shared/FlexContainer.tsx
+++ src/components/styled/shared/FlexContainer.tsx
@@ -1,16 +1,26 @@
 import styled from 'styled-components';
 
 interface IFlexContainerProps {
-  align?: string;
-  justify?: string;
-  direction?: string;
+  $align?: string;
+  $justify?: string;
+  $direction?: string;
   $gap?: string;
+  $maxWidth?: string;
+  $width?: string;
+  $padding?: string;
+  $margin?: string;
+  $flexWrap?: string;
 }
 
 export const FlexContainer = styled.div<IFlexContainerProps>`
   display: flex;
-  align-items: ${(props) => props.align || 'center'};
-  justify-content: ${(props) => props.align || 'center'};
-  flex-direction: ${(props) => props.align || 'row'};
+  align-items: ${(props) => props.$align || 'center'};
+  justify-content: ${(props) => props.$justify || 'center'};
+  flex-direction: ${(props) => props.$direction || 'row'};
   gap: ${(props) => props.$gap || '0'};
+  width: ${(props) => props.$width || 'auto'};
+  padding: ${(props) => props.$padding || '0'};
+  max-width: ${(props) => props.$maxWidth || '100%'};
+  flex-wrap: ${(props) => props.$flexWrap || 'nowrap'};
+  margin: ${(props) => props.$margin || '0'};
 `;
