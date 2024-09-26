b3453e3e3c5c8cdfa58891e04d55a96dd8ad85c2
RalfiSlask <nilssonnarki@gmail.com>
2024-09-26 17:21:51 +0200

    ✨ add function for getting days left based on an isoDate, and for getting day & month from iso date

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -8,3 +8,17 @@ export const formatToSwedishDate = (isoDate: string) => {
 
   return `${day} ${month} ${year}, kl. ${hours}.${minutes}`;
 };
+
+export const getDaysLeft = (isoDate: string) => {
+  const currentDate = new Date();
+  const deadlineDate = new Date(isoDate);
+  const timeDifference = deadlineDate.getTime() - currentDate.getTime();
+  return Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+};
+
+export const getDayAndMonthFromIsoString = (isoDate: string) => {
+  const date = new Date(isoDate);
+  const day = date.getDate();
+  const month = date.toLocaleString('sv-SE', { month: 'short' });
+  return `${day} ${month}`;
+};
