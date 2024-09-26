25e1cb62dd4d8ddf6637ade988d3204b0c79dca0
armin-164 <arminsadr164@hotmail.com>
2024-09-27 01:08:34 +0200

    feat: add image components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,7 @@ import {
 import {
   DigiFormInputSearch,
   DigiLayoutBlock,
+  DigiMediaImage,
 } from '@digi/arbetsformedlingen-react';
 
 export const Home = () => {
@@ -25,7 +26,18 @@ export const Home = () => {
             className="home-search-container"
           ></DigiFormInputSearch>
         </section>
-        <section className="home-bottom-section"></section>
+        <section className="home-bottom-section">
+          <DigiMediaImage
+            afUnlazy
+            afSrc="https://images.unsplash.com/photo-1607134541550-2994abb8077b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            afAlt="construction worker using a survey equipment"
+          ></DigiMediaImage>
+          <DigiMediaImage
+            afUnlazy
+            afSrc="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            afAlt="office workers looking at paperwork"
+          ></DigiMediaImage>
+        </section>
       </DigiLayoutBlock>
     </>
   );
