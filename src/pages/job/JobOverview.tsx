84efa42cc176d994aa81d055ff2bed1dda24e6fb
RalfiSlask <nilssonnarki@gmail.com>
2024-09-26 19:58:12 +0200

    ✨ add Job Overview (left section) containing job description and information

---
+++ src/pages/job/JobOverview.tsx
@@ -0,0 +1,19 @@
+import { DigiLayoutBlock } from '@digi/arbetsformedlingen-react';
+import JobDescription from './JobDescription';
+import JobInfo from './JobInfo';
+import { FlexContainer } from '../../components/styled/shared/FlexContainer';
+
+const JobOverview = () => {
+  return (
+    <FlexContainer $maxWidth="720px" $width="100%">
+      <DigiLayoutBlock afMarginBottom={true}>
+        <FlexContainer $direction="column" $align="flex-start" $gap="20px">
+          <JobDescription />
+          <JobInfo />
+        </FlexContainer>
+      </DigiLayoutBlock>
+    </FlexContainer>
+  );
+};
+
+export default JobOverview;
