4e888cb38d404d4863cbdedafc513130e5d99173
Diem Bang <diembang09@gmail.com>
2024-09-26 23:48:22 +0200

    :lipstick: Update publication date format

--- src/pages/searchPage/SearchResult.tsx
+++ src/pages/searchPage/SearchResult.tsx
@@ -1,34 +1,40 @@
+import { TypographyTimeVariation } from "@digi/arbetsformedlingen";
+import { DigiTypographyTime } from "@digi/arbetsformedlingen-react";
 import { SearchResultWrapper } from "../../components/styled/Wrappers";
 
 interface IEmployer {
-    name: string;
+  name: string;
 }
 
 interface IWorkplaceAddress {
-    city: string | null;
+  city: string | null;
 }
 
 interface IWorkinghoursType {
-    label: string;
+  label: string;
 }
 
 export interface IAd {
-    id: string;
-    headline: string;
-    working_hours_type: IWorkinghoursType;
-    employer: IEmployer;
-    workplace_address: IWorkplaceAddress;
-    publication_date: string;
-  }
-
-export const SearchResult = (props:IAd) => {
-    return (
-        <SearchResultWrapper>
-        <h3>{props.headline}</h3>
-        <h4>{props.employer.name} - {props.workplace_address.city}</h4>
-        <p>{props.working_hours_type.label}</p>
-        <p>Publicerad {props.publication_date}</p>
-        
-        </SearchResultWrapper>
-    );
+  id: string;
+  headline: string;
+  working_hours_type: IWorkinghoursType;
+  employer: IEmployer;
+  workplace_address: IWorkplaceAddress;
+  publication_date: string;
 }
+
+export const SearchResult = (props: IAd) => {
+  return (
+    <SearchResultWrapper>
+      <h3>{props.headline}</h3>
+      <h4>
+        {props.employer.name} - {props.workplace_address.city}
+      </h4>
+      <p>{props.working_hours_type.label}</p>
+      <p>Publicerad <DigiTypographyTime
+        afVariation={TypographyTimeVariation.PRETTY}
+        afDateTime={props.publication_date}
+      ></DigiTypographyTime></p>
+    </SearchResultWrapper>
+  );
+};
