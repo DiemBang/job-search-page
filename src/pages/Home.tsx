77cee0a50d20bace434a2a26c733542232d745ad
armin-164 <arminsadr164@hotmail.com>
2024-09-27 23:46:29 +0200

    feat: set Unlazy to true so lazyloading is off for specified imgs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -41,12 +41,12 @@ export const Home = () => {
         </section>
         <section className="home-bottom-section">
           <DigiMediaImage
-            afUnlazy
+            afUnlazy={true}
             afSrc="https://images.unsplash.com/photo-1607134541550-2994abb8077b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             afAlt="construction worker using a survey equipment"
           ></DigiMediaImage>
           <DigiMediaImage
-            afUnlazy
+            afUnlazy={true}
             afSrc="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             afAlt="office workers looking at paperwork"
           ></DigiMediaImage>
