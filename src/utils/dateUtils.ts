5bee6e9257717c31fa92f297ffd013a666a87366
RalfiSlask <nilssonnarki@gmail.com>
2024-09-27 14:04:41 +0200

    ✨ change formatIsoDate function so it returns only time now

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,12 +1,9 @@
-export const formatToSwedishDate = (isoDate: string) => {
+export const formatToTimeFromIsoDate = (isoDate: string) => {
   const date = new Date(isoDate);
-  const day = date.getDate();
-  const month = date.toLocaleString('sv-SE', { month: 'long' });
-  const year = date.getFullYear();
   const hours = date.getHours().toString().padStart(2, '0');
   const minutes = date.getMinutes().toString().padStart(2, '0');
 
-  return `${day} ${month} ${year}, kl. ${hours}.${minutes}`;
+  return `kl. ${hours}.${minutes}`;
 };
 
 export const getDaysLeft = (isoDate: string) => {
