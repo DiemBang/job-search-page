fc35ff45d4f1230f81130ab5b3c89606c73e35dc
RalfiSlask <nilssonnarki@gmail.com>
2024-09-28 09:04:52 +0200

    ✨ made sure job page loader now is working as expected

--- src/loaders/jobPageLoader.ts
+++ src/loaders/jobPageLoader.ts
@@ -1,11 +1,19 @@
-import axios from "axios";
-import { IJobPageLoader } from "../models/IJobPageLoader";
+import { IJobPageLoader } from '../models/IJobPageLoader';
+import { IOccupation } from '../types/occupation-types';
+import { getBase } from '../services/serviceBase';
 
-const BASE_URL = "https://jobsearch.api.jobtechdev.se/ad/";
+const BASE_URL = 'https://jobsearch.api.jobtechdev.se/ad/';
 
-export const jobPageLoader = async ({params}: IJobPageLoader) => {
-    let response = await axios.get(`${BASE_URL}${params.id}`);
-    console.log(response.data);
-
-    return response;
-}
+export const jobPageLoader = async ({
+  params,
+}: IJobPageLoader): Promise<IOccupation | null> => {
+  try {
+    const occupationUrl = `${BASE_URL}${params.id}`;
+    const occupationData = await getBase<IOccupation>(occupationUrl);
+    console.log('this is the response with occupation: ', occupationData);
+    return occupationData;
+  } catch (err) {
+    console.log('error fetching occupation data: ', err);
+    return null;
+  }
+};
