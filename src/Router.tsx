bf1bf165c5bf46c50b2e235f8cf5173ba0d5f0cd
RalfiSlask <nilssonnarki@gmail.com>
2024-09-28 09:03:53 +0200

    ✨ fix Router so we have all relevant imports

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,25 +1,28 @@
-import { createBrowserRouter } from "react-router-dom";
-import { NotFound } from "./pages/NotFound";
-import { SearchPage } from "./pages/searchPage/SearchPage";
-import { jobPageLoader } from "./loaders/jobPageLoader";
-import { searchPageLoader } from "./loaders/searchPageLoader";
-import { DigiLoaderSpinner } from "@digi/arbetsformedlingen-react";
-import { Suspense } from "react";
-import { LoaderSpinnerSize } from "@digi/arbetsformedlingen";
+import { createBrowserRouter } from 'react-router-dom';
+import { NotFound } from './pages/NotFound';
+import { SearchPage } from './pages/searchPage/SearchPage';
+import { jobPageLoader } from './loaders/jobPageLoader';
+/* import { searchPageLoader } from './loaders/searchPageLoader'; */
+import { DigiLoaderSpinner } from '@digi/arbetsformedlingen-react';
+import { Suspense } from 'react';
+import { LoaderSpinnerSize } from '@digi/arbetsformedlingen';
+import { Layout } from './pages/Layout';
+import { Home } from './pages/home/Home';
+import Job from './pages/job/Job';
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Layout></Layout>,
     errorElement: <NotFound></NotFound>,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Home></Home>,
         index: true,
       },
       {
-        path: "/search",
+        path: '/search',
         element: (
           <Suspense
             fallback={
@@ -32,11 +35,11 @@ export const router = createBrowserRouter([
             <SearchPage></SearchPage>
           </Suspense>
         ),
-        loader: searchPageLoader,
+        /*  loader: searchPageLoader, */
       },
       {
-        path: "/job/:id",
-        element: <JobPage></JobPage>,
+        path: '/search/job/:id',
+        element: <Job></Job>,
         loader: jobPageLoader,
       },
     ],
