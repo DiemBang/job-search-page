3d216a2d06e10a6155957c98d98d98d329255f87
armin-164 <arminsadr164@hotmail.com>
2024-09-29 16:52:35 +0200

    feat: add components and elements
    Wrap the elements with a DigiTypography component
    and replace img element with img component from Digi Core.
    Also add a href to return back to the homepage.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,19 @@
+import { DigiMediaImage, DigiTypography } from '@digi/arbetsformedlingen-react';
+import pageNotFoundImg from "../assets/404-error.png";
+
 export const NotFound = () => {
   return (
     <>
-      <h1>404.</h1>
-      <h2>
-        Hoppsan, nu blev det lite galet. Sidan du försöker hitta finns inte.
-      </h2>
+      <DigiTypography>
+        <h1>404 - Sidan kunde inte hittas</h1>
+        <p>Tyvärr, sidan du letar efter finns inte.</p>
+        <DigiMediaImage
+          afUnlazy={true}
+          afSrc={pageNotFoundImg}
+          afAlt="construction worker using a survey equipment"
+        ></DigiMediaImage>{' '}
+        <a href="/">Gå tillbaka till startsidan</a>
+      </DigiTypography>
     </>
   );
 };
