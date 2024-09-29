eacf4d71d6f9250d45795a8360213603f1b3b22f
Diem Bang <diembang09@gmail.com>
2024-09-29 22:57:51 +0200

    :alien: Replace mock data with api

--- src/pages/searchPage/DisplaySearchResults.tsx
+++ src/pages/searchPage/DisplaySearchResults.tsx
@@ -1,22 +1,32 @@
 import { useEffect, useState } from 'react';
 import { IAd, SearchResult } from './SearchResult';
-import { searchData } from '../../data/search-data';
+import axios from "axios";
 
 export const DisplaySearchResults = () => {
   const [ads, setAds] = useState<IAd[]>([]);
+  const [fetched, setFetched] = useState(false);
+  const [totalAds, setTotalAds] = useState(0);
+  const [totalPositions, setTotalPositions] = useState(0);
 
   useEffect(() => {
+    if (fetched) return;
     const getData = async () => {
-      const data: IAd[] = searchData.hits;
-      setAds(data);
+      let response = await axios.get(
+        "https://jobsearch.api.jobtechdev.se/search?offset=0&limit=20"
+      );
+      setAds(response.data.hits);
+      setTotalAds(response.data.total.value);
+      setTotalPositions(response.data.positions);
+      setFetched(true);
     };
+
     getData();
   });
 
   return (
     <>
       <h3>
-        {searchData.total.value} annonser med {searchData.positions} jobb
+        {totalAds} annonser med {totalPositions} jobb
         hittades
       </h3>
       {ads.map((ad) => (
