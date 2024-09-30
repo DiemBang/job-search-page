aa3962b30cd526083cff6f6f8d2c5e7ca707bd7c
armin-164 <arminsadr164@hotmail.com>
2024-09-30 11:25:22 +0200

    feat: Add searchPageLoader to fetch occupations data

--- src/loaders/searchPageLoader.ts
+++ src/loaders/searchPageLoader.ts
@@ -1,3 +1,25 @@
-export const searchPageLoader = async () => {
+import { getBase } from "../services/serviceBase";
+import { IOccupations } from "../types/occupation-types";
+
+const BASE_URL = 'https://jobsearch.api.jobtechdev.se/search?offset=0&limit=10';
+
+
+export const searchPageLoader = async ({request}: {request: Request}): Promise<IOccupations | null> => {
+    const url = new URL(request.url);
+    const freeSearch = url.searchParams.get("q");
+
+    try {
+        const occupationUrl = `${BASE_URL}&q=${freeSearch}`;
+        const occupationsData = await getBase<IOccupations>(occupationUrl);
+
+        return occupationsData;
+
+    }
+
+    catch (err) {
+        console.log("Error fetching occupations:", err)
+        return null;
+    }
+
     
 }
