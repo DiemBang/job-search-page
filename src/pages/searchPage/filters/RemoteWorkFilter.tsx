d0a9b9d79859bef5a17a4888f23735779da6f15e
Diem Bang <diembang09@gmail.com>
2024-10-01 16:05:49 +0200

    :bug: Remove checkbox under filter

--- src/pages/searchPage/filters/RemoteWorkFilter.tsx
+++ src/pages/searchPage/filters/RemoteWorkFilter.tsx
@@ -7,39 +7,34 @@ export const RemoteWorkFilter = () => {
   const { remoteWorkplace, setRemoteWorkplace } = useContext(FilterContext);
   const { createFilterParams, getData } = useContext(AdsContext);
 
-  const changeToRemoteWorkplace = (id: string, isChecked: boolean) => {
-    if (id === "endast_distans") {
-      setRemoteWorkplace(isChecked);
-    }
-  }
+  const changeToRemoteWorkplace = (isChecked: boolean) => {
+    setRemoteWorkplace(isChecked);
+  };
 
   // Create filter params after each change of remote workplace
   useEffect(() => {
     let filterParams = createFilterParams();
     getData(filterParams);
-  }, [remoteWorkplace])
+  }, [remoteWorkplace]);
 
-    return (
-        <>
-        <DigiFormFilter
-          afFilterButtonText="Arbetsplats"
-          afSubmitButtonText="Filtrera"
-          afListItems={[
-            { id: "alla", label: "Alla" },
-            { id: "endast_distans", label: "Endast distansarbete" },
-
-          ]}
-          afCheckItems={["alla"]} // optional, override internal check state of component with filter ids
-          onAfChangeFilter={(e) => changeToRemoteWorkplace(e.detail.id, e.detail.isChecked)}
-          onAfResetFilter={() => console.log("reset filter")}
-          onAfSubmitFilter={(e) =>
-            console.log("submit filter", e.detail.listItems, e.detail.checked)
-          }
-          onAfCloseFilter={(e) =>
-            console.log("submit filter", e.detail.listItems, e.detail.checked)
-          }
-        ></DigiFormFilter>
-        
-        </>
-    )
-}
+  return (
+    <>
+      <DigiFormFilter
+        afFilterButtonText="Arbetsplats"
+        afSubmitButtonText="Filtrera"
+        afListItems={[
+          // { id: "alla", label: "Alla" },
+          { id: "endast_distans", label: "Endast distansarbete" },
+        ]}
+        onAfChangeFilter={(e) => changeToRemoteWorkplace(e.detail.isChecked)}
+        onAfResetFilter={() => console.log("reset filter")}
+        onAfSubmitFilter={(e) =>
+          console.log("submit filter", e.detail.listItems, e.detail.checked)
+        }
+        onAfCloseFilter={(e) =>
+          console.log("submit filter", e.detail.listItems, e.detail.checked)
+        }
+      ></DigiFormFilter>
+    </>
+  );
+};
