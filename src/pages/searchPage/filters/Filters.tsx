6bde858e37624c1c6e14088534997ebae676e676
RalfiSlask <nilssonnarki@gmail.com>
2024-10-01 17:54:31 +0200

    ✨ conditionally render an overlay depending on if a dropdown is open, on click we close all dropdowns.

--- src/pages/searchPage/filters/Filters.tsx
+++ src/pages/searchPage/filters/Filters.tsx
@@ -1,19 +1,26 @@
-import { LocationFilter } from "./LocationFilter";
-import { OccupationFilter } from ".//OccupationFilter";
-import { WorkTimeExtentFilter } from "./WorktimeExtentFilter";
-import { EmploymentTypeFilter } from "./EmploymentTypeFilter";
-import { RemoteWorkFilter } from "./RemoteWorkFilter";
-import { DriverLicenseFilter } from "./DriverLicenseFilter";
-import { EducationFilter } from "./EducationFilter";
-import { LanguageFilter } from "./LanguageFilter";
-import { PublicationFilter } from "./PublicationFilter";
-import { FilterWrapper } from "../../../components/styled/Wrappers";
+import { LocationFilter } from './region-filter/LocationFilter';
+import { OccupationFilter } from './occupation-filter/OccupationFilter';
+import { WorkTimeExtentFilter } from './WorktimeExtentFilter';
+import { EmploymentTypeFilter } from './EmploymentTypeFilter';
+import { RemoteWorkFilter } from './RemoteWorkFilter';
+import { DriverLicenseFilter } from './DriverLicenseFilter';
+import { EducationFilter } from './EducationFilter';
+import { LanguageFilter } from './LanguageFilter';
+import { PublicationFilter } from './PublicationFilter';
+import { FilterWrapper } from '../../../components/styled/Wrappers';
+import { OverlayContainer } from '../../../components/styled/shared/OverlayContainer';
+import useModalsContext from '../../../hooks/useModalsContext';
 
 export const Filters = () => {
+  const { isDropDownsOpen, closeAllDropDowns } = useModalsContext();
+
   return (
     <section>
-        <h3>Filtrera din sökning</h3>
-      <FilterWrapper>        
+      <h3>Filtrera din sökning</h3>
+      {isDropDownsOpen.overlay && (
+        <OverlayContainer onClick={closeAllDropDowns} />
+      )}
+      <FilterWrapper>
         <LocationFilter />
         <OccupationFilter />
         <WorkTimeExtentFilter />
