414f1d301002f26e10edf14197922bb25bdcdb11
RalfiSlask <nilssonnarki@gmail.com>
2024-10-02 19:27:00 +0200

    ✨ improve loader with better logic and added query for sort

--- src/loaders/searchPageLoader.ts
+++ src/loaders/searchPageLoader.ts
@@ -1,25 +1,32 @@
-import { getBase } from "../services/serviceBase";
-import { IOccupations } from "../types/occupation-types";
+import { getBase } from '../services/serviceBase';
+import { IOccupations } from '../types/occupation-types';
 
 const BASE_URL = 'https://jobsearch.api.jobtechdev.se/search?offset=0&limit=10';
 
+export const searchPageLoader = async ({
+  request,
+}: {
+  request: Request;
+}): Promise<IOccupations | null> => {
+  const url = new URL(request.url);
+  const freeSearch = url.searchParams.get('q');
+  const sort = url.searchParams.get('sort');
+  let occupationUrl = BASE_URL;
 
-export const searchPageLoader = async ({request}: {request: Request}): Promise<IOccupations | null> => {
-    const url = new URL(request.url);
-    const freeSearch = url.searchParams.get("q");
+  if (freeSearch?.length) {
+    occupationUrl += `&q=${freeSearch}`;
+  }
+  if (sort) {
+    occupationUrl += `&sort=${sort}`;
+  }
 
-    try {
-        const occupationUrl = `${BASE_URL}&q=${freeSearch}`;
-        const occupationsData = await getBase<IOccupations>(occupationUrl);
+  console.log('this is occupationUrl:', occupationUrl);
 
-        return occupationsData;
-
-    }
-
-    catch (err) {
-        console.log("Error fetching occupations:", err)
-        return null;
-    }
-
-    
-}
+  try {
+    const occupationsData = await getBase<IOccupations>(occupationUrl);
+    return occupationsData;
+  } catch (err) {
+    console.log('Error fetching occupations:', err);
+    return null;
+  }
+};
