32d750291a67b166163e460cf90abb60c349d462
RalfiSlask <nilssonnarki@gmail.com>
2024-10-03 07:11:05 +0200

    ✨ remove getBaseCopy (we dont need it anymore logic in context instead)

--- src/services/serviceBase.ts
+++ src/services/serviceBase.ts
@@ -1,29 +1,7 @@
-import axios, { AxiosResponse } from 'axios';
-import { IAd } from '../pages/searchPage/SearchResult';
+import axios from 'axios';
 
-const BASE_URL = 'https://jobsearch.api.jobtechdev.se/search?offset=0&limit=20';
 export const getBase = async <T>(url: string) => {
+  console.log('this is the url recievied in service base: ', url);
   const response = await axios.get<T>(url);
-  return response.data;
-};
-
-interface ITotalAds {
-  value: number;
-}
-interface IAdResponseData {
-  hits: IAd[];
-  total: ITotalAds;
-  positions: number;
-}
-
-export const getBaseCopy = async (
-  params: URLSearchParams | null
-): Promise<IAdResponseData> => {
-  let response: AxiosResponse;
-  if (params === null) {
-    response = await axios.get<IAdResponseData>(BASE_URL);
-  } else {
-    response = await axios.get<IAdResponseData>(`${BASE_URL}&${params}`);
-  }
   return response.data;
 };
