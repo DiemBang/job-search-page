aa618364862ecde8bb0d2b98088b930a7f5e1d47
armin-164 <arminsadr164@hotmail.com>
2024-10-03 10:07:33 +0200

    feat: calculate offsetvalue based on page

--- src/loaders/searchPageLoader.ts
+++ src/loaders/searchPageLoader.ts
@@ -1,15 +1,19 @@
 import { getBase } from "../services/serviceBase";
 import { IOccupations } from "../types/occupation-types";
 
-const BASE_URL = 'https://jobsearch.api.jobtechdev.se/search?offset=0&limit=10';
+const BASE_URL = 'https://jobsearch.api.jobtechdev.se/search?limit=10';
 
 
 export const searchPageLoader = async ({request}: {request: Request}): Promise<IOccupations | null> => {
     const url = new URL(request.url);
     const freeSearch = url.searchParams.get("q");
+    const pageValue = url.searchParams.get("page");
+
+    const page: number = pageValue ? parseInt(pageValue) : 1;    
+    const offsetValue = (page - 1) * 10;
 
     try {
-        const occupationUrl = `${BASE_URL}&q=${freeSearch}`;
+        const occupationUrl = `${BASE_URL}&offset=${offsetValue}&q=${freeSearch}`;
         const occupationsData = await getBase<IOccupations>(occupationUrl);
 
         return occupationsData;
