0a9bcabab2accc03c07ef711c3506a561d767e38
armin-164 <arminsadr164@hotmail.com>
2024-10-06 16:18:51 +0200

    feat: wrap return value  with DigiTypography component

--- src/pages/searchPage/DisplaySearchResults.tsx
+++ src/pages/searchPage/DisplaySearchResults.tsx
@@ -2,26 +2,28 @@ import { SearchResult } from './SearchResult';
 import useAdvertsContext from '../../hooks/useAdvertsContext';
 import { FlexContainer } from '../../components/styled/shared/FlexContainer';
 import SortAds from './SortAds';
+import { DigiTypography } from '@digi/arbetsformedlingen-react';
 
 export const DisplaySearchResults = () => {
   const { ads, totalAds, totalPositions } = useAdvertsContext();
 
   return (
     <>
-      <FlexContainer
-        $justify="space-between"
-        $padding="32px 0"
-        $align="flex-start"
-      >
-        <h3>
-          {totalAds} annonser med {totalPositions} jobb hittades
-        </h3>
-        <SortAds />
-      </FlexContainer>
-
-      {ads.map((ad) => (
-        <SearchResult key={ad.id} ad={ad} />
-      ))}
+      <DigiTypography>
+        <FlexContainer
+          $justify="space-between"
+          $padding="32px 0"
+          $align="flex-start"
+        >
+          <h3>
+            {totalAds} annonser med {totalPositions} jobb hittades
+          </h3>
+          <SortAds />
+        </FlexContainer>
+        {ads.map((ad) => (
+          <SearchResult key={ad.id} ad={ad} />
+        ))}
+      </DigiTypography>
     </>
   );
 };
