9aa94a63480fd9d7fd2caf058a64e6a498def751
Diem Bang <72154226+DiemBang@users.noreply.github.com>
2024-10-07 16:16:45 +0200

    :construction: Add useEffect to check if there is already a saved list in local storage

--- src/pages/searchPage/AddFavourite.tsx
+++ src/pages/searchPage/AddFavourite.tsx
@@ -1,35 +1,56 @@
 import { DigiIconStar, DigiIconStarReg } from "@digi/arbetsformedlingen-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IAdProps } from "./SearchResult";
 
-export const AddFavourite = ({ad}: IAdProps) => {
-    const [isFavourite, setIsFavourite] = useState(false);
+export const AddFavourite = ({ ad }: IAdProps) => {
+  const [isFavourite, setIsFavourite] = useState(false);
 
-    const toggleFavourite = () => {
-        
-        console.log(ad.id);
-        const savedFavourites = JSON.parse(localStorage.getItem("savedFavouritesList") || "[]");
-
-        let updatedFavourites;
-        if (isFavourite) {
-            // Remove ad.id from the list if it was already a favourite
-            updatedFavourites = savedFavourites.filter((favId: string) => favId !== ad.id);
-        } else {
-            // Add ad.id to the list if it's not a favourite yet
-            updatedFavourites = [...savedFavourites, ad.id];
-        }
-
-         // Update local storage with the new list
-         localStorage.setItem("savedFavouritesList", JSON.stringify(updatedFavourites));
-
-         // Toggle the favourite state
-         setIsFavourite(!isFavourite);
-    }
-    return (
-        <>
-        <p>Spara annons</p>
-        {!isFavourite && <DigiIconStarReg afSvgAriaLabelledby="star icon symbol" afSvgAriaHidden={true} onClick={toggleFavourite}></DigiIconStarReg>}
-        {isFavourite && <DigiIconStar afSvgAriaLabelledby="star icon symbol clicked" afSvgAriaHidden onClick={toggleFavourite}></DigiIconStar>}
-        </>
+  useEffect(() => {
+    const savedFavourites = JSON.parse(
+      localStorage.getItem("savedFavouritesList") || "[]"
     );
-}
+    setIsFavourite(savedFavourites.includes(ad.id));
+  }, [ad.id]);
+
+  const toggleFavourite = () => {
+    console.log(ad.id);
+    const savedFavourites = JSON.parse(
+      localStorage.getItem("savedFavouritesList") || "[]"
+    );
+
+    let updatedFavourites;
+    if (isFavourite) {
+      updatedFavourites = savedFavourites.filter(
+        (favId: string) => favId !== ad.id
+      );
+    } else {
+      updatedFavourites = [...savedFavourites, ad.id];
+    }
+
+    localStorage.setItem(
+      "savedFavouritesList",
+      JSON.stringify(updatedFavourites)
+    );
+
+    setIsFavourite(!isFavourite);
+  };
+  return (
+    <>
+      <p>Spara annons</p>
+      {!isFavourite && (
+        <DigiIconStarReg
+          afSvgAriaLabelledby="star icon symbol"
+          afSvgAriaHidden={true}
+          onClick={toggleFavourite}
+        ></DigiIconStarReg>
+      )}
+      {isFavourite && (
+        <DigiIconStar
+          afSvgAriaLabelledby="star icon symbol clicked"
+          afSvgAriaHidden
+          onClick={toggleFavourite}
+        ></DigiIconStar>
+      )}
+    </>
+  );
+};
