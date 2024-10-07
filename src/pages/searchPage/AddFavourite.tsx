f377d696d84a71bec91435a1722e74f0bc44f7eb
Diem Bang <72154226+DiemBang@users.noreply.github.com>
2024-10-07 12:56:09 +0200

    :construction: Add star icons

--- src/pages/searchPage/AddFavourite.tsx
+++ src/pages/searchPage/AddFavourite.tsx
@@ -1,10 +1,11 @@
-import { DigiIconStarReg } from "@digi/arbetsformedlingen-react";
+import { DigiIconStar, DigiIconStarReg } from "@digi/arbetsformedlingen-react";
 
 export const AddFavourite = () => {
     return(
         <>
         <p>Spara annons</p>
-        <DigiIconStarReg></DigiIconStarReg>
+        <DigiIconStarReg afSvgAriaLabelledby="star icon symbol" afSvgAriaHidden="false"></DigiIconStarReg>
+        <DigiIconStar afSvgAriaLabelledby="star icon symbol clicked" afSvgAriaHidden></DigiIconStar>
         </>
     );
 }
