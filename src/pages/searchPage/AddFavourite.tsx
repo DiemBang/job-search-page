5c2cc9ff2f479ff8c5fec5d1351a562a2f0bf2c8
Diem Bang <72154226+DiemBang@users.noreply.github.com>
2024-10-07 16:12:14 +0200

    :sparkles: Add save to local storage functionality

--- src/pages/searchPage/AddFavourite.tsx
+++ src/pages/searchPage/AddFavourite.tsx
@@ -1,11 +1,35 @@
 import { DigiIconStar, DigiIconStarReg } from "@digi/arbetsformedlingen-react";
+import { useState } from "react";
+import { IAdProps } from "./SearchResult";
 
-export const AddFavourite = () => {
+export const AddFavourite = ({ad}: IAdProps) => {
+    const [isFavourite, setIsFavourite] = useState(false);
+
+    const toggleFavourite = () => {
+        
+        console.log(ad.id);
+        const savedFavourites = JSON.parse(localStorage.getItem("savedFavouritesList") || "[]");
+
+        let updatedFavourites;
+        if (isFavourite) {
+            // Remove ad.id from the list if it was already a favourite
+            updatedFavourites = savedFavourites.filter((favId: string) => favId !== ad.id);
+        } else {
+            // Add ad.id to the list if it's not a favourite yet
+            updatedFavourites = [...savedFavourites, ad.id];
+        }
+
+         // Update local storage with the new list
+         localStorage.setItem("savedFavouritesList", JSON.stringify(updatedFavourites));
+
+         // Toggle the favourite state
+         setIsFavourite(!isFavourite);
+    }
     return (
         <>
         <p>Spara annons</p>
-        <DigiIconStarReg afSvgAriaLabelledby="star icon symbol" afSvgAriaHidden="false"></DigiIconStarReg>
-        <DigiIconStar afSvgAriaLabelledby="star icon symbol clicked" afSvgAriaHidden></DigiIconStar>
+        {!isFavourite && <DigiIconStarReg afSvgAriaLabelledby="star icon symbol" afSvgAriaHidden={true} onClick={toggleFavourite}></DigiIconStarReg>}
+        {isFavourite && <DigiIconStar afSvgAriaLabelledby="star icon symbol clicked" afSvgAriaHidden onClick={toggleFavourite}></DigiIconStar>}
         </>
     );
 }
--- src/pages/searchPage/SearchResult.tsx
+++ src/pages/searchPage/SearchResult.tsx
@@ -31,24 +31,26 @@ export interface IAdProps {
 
 export const SearchResult = ({ ad }: IAdProps) => {
   return (
-    <StyledRouterLink to={`/search/job/${ad.id}`}>
-      <SearchResultWrapper>
-        <h3>{ad.headline}</h3>
-        <h4>
-          {ad.employer.name}
-          {ad.employer.name && ad.workplace_address.municipality && <> - </>}
-          {ad.workplace_address.municipality}
-        </h4>
-        <p>{ad.working_hours_type.label}</p>
-        <p>
-          Publicerad{" "}
-          <DigiTypographyTime
-            afVariation={TypographyTimeVariation.PRETTY}
-            afDateTime={ad.publication_date}
-          ></DigiTypographyTime>
-        </p>
-        <AddFavourite></AddFavourite>
-      </SearchResultWrapper>
-    </StyledRouterLink>
+    <>
+      <StyledRouterLink to={`/search/job/${ad.id}`}>
+        <SearchResultWrapper>
+          <h3>{ad.headline}</h3>
+          <h4>
+            {ad.employer.name}
+            {ad.employer.name && ad.workplace_address.municipality && <> - </>}
+            {ad.workplace_address.municipality}
+          </h4>
+          <p>{ad.working_hours_type.label}</p>
+          <p>
+            Publicerad{" "}
+            <DigiTypographyTime
+              afVariation={TypographyTimeVariation.PRETTY}
+              afDateTime={ad.publication_date}
+            ></DigiTypographyTime>
+          </p>
+        </SearchResultWrapper>
+      </StyledRouterLink>
+      <AddFavourite ad = {ad}></AddFavourite>
+    </>
   );
 };
