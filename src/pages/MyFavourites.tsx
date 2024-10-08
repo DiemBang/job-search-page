607baee59177accbeeb63030ca131f1e8c7d9a15
Diem Bang <72154226+DiemBang@users.noreply.github.com>
2024-10-08 16:19:56 +0200

    :lipstick: Add typography component

--- src/pages/MyFavourites.tsx
+++ src/pages/MyFavourites.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { SearchResult } from "./searchPage/SearchResult";
 import { IOccupation } from "../types/occupation-types";
+import { DigiTypography } from "@digi/arbetsformedlingen-react";
 
 export const MyFavourites = () => {
   const [favourites, setFavourites] = useState<IOccupation[]>([]);
@@ -15,16 +16,18 @@ export const MyFavourites = () => {
 
   return (
     <>
-      <h2>Mina sparade annonser</h2>
-      {favourites.length === 0 ? (
-        <p>Du har inga sparade annonser</p>
-      ) : (
-        <>
-          {favourites.map((favAd) => (
-            <SearchResult key={favAd.id} ad={favAd}></SearchResult>
-          ))}
-        </>
-      )}
+      <DigiTypography>
+        <h2>Mina sparade annonser</h2>
+        {favourites.length === 0 ? (
+          <p>Du har inga sparade annonser</p>
+        ) : (
+          <>
+            {favourites.map((favAd) => (
+              <SearchResult key={favAd.id} ad={favAd}></SearchResult>
+            ))}
+          </>
+        )}
+      </DigiTypography>
     </>
   );
 };
