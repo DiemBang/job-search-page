cb44dad1b96fd4ed336663d074dd6d5fd2089af4
armin-164 <arminsadr164@hotmail.com>
2024-10-08 19:22:41 +0200

    feat: add dynamic values for current result of pagination depending on page number

--- src/components/shared/Pagination.tsx
+++ src/components/shared/Pagination.tsx
@@ -1,6 +1,7 @@
 import { DigiNavigationPagination } from "@digi/arbetsformedlingen-react";
 import { DigiNavigationPaginationCustomEvent } from "@digi/arbetsformedlingen/dist/types/components";
 import useAdvertsContext from "../../hooks/useAdvertsContext";
+import { useState } from "react";
 
 interface PaginationProps {
     totalPages: number;
@@ -9,18 +10,23 @@ interface PaginationProps {
 
 export const Pagination = ({ totalPages, totalResults }: PaginationProps) => {
   const { handleClickOnPaginationButton } = useAdvertsContext();
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const currentResultStart = (currentPage - 1) * 20 + 1;
+  const currentResultEnd = currentPage * 20;
 
   const handlePageChange = (
     e: DigiNavigationPaginationCustomEvent<number>
   ): void => {
+    setCurrentPage(e.detail);
     handleClickOnPaginationButton(e.detail);
   };
 
   return (
     <>
       <DigiNavigationPagination
-        afCurrentResultStart={20}
-        afCurrentResultEnd={20}
+        afCurrentResultStart={currentResultStart}
+        afCurrentResultEnd={currentResultEnd}
         afInitActivePage={1}
         afLimit={10}
         afTotalPages={totalPages}
