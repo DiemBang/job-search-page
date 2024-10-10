8b46d23ad1540352c705f87c248e573fed46aaa7
armin-164 <arminsadr164@hotmail.com>
2024-10-10 13:02:59 +0200

    refactor: remove unnecessary code

--- src/pages/searchPage/SortAds.tsx
+++ src/pages/searchPage/SortAds.tsx
@@ -1,7 +1,7 @@
 import { FormSelectVariation } from '@digi/arbetsformedlingen';
 import { DigiFormSelect } from '@digi/arbetsformedlingen-react';
 import useAdvertsContext from '../../hooks/useAdvertsContext';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { DigiFormSelectCustomEvent } from '@digi/arbetsformedlingen/dist/types/components';
 
 const SortAds = () => {
@@ -19,21 +19,6 @@ const SortAds = () => {
     selectValue = 'Relevans';
   }
   const [selectedOption, setSelectedOption] = useState(selectValue);
-
-  useEffect(() => {
-    // check URL to see if needs to add to checkedList
-    const urlParams = new URLSearchParams(window.location.search);
-    const sortParam = urlParams.get("sort");
-    if (sortParam) {
-      setSelectedOption(sortParam);
-    }
-    console.log(selectedOption, "test");
-
-  }, []);
-  
-  useEffect(() => {
-    console.log("Updated selected option:", selectedOption);
-  }, [selectedOption]);
 
   const handleSelectChange = (
     e: DigiFormSelectCustomEvent<HTMLDigiFormSelectElement>
