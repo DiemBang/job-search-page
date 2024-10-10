32a0d068011b2e55a450e9ecb7c137db9af5ccbe
RalfiSlask <nilssonnarki@gmail.com>
2024-10-10 08:21:23 +0200

    ✨ made sure we get the correct value for sorting when we refresh page by using query value for it

--- src/pages/searchPage/SortAds.tsx
+++ src/pages/searchPage/SortAds.tsx
@@ -5,8 +5,20 @@ import { useState } from 'react';
 import { DigiFormSelectCustomEvent } from '@digi/arbetsformedlingen/dist/types/components';
 
 const SortAds = () => {
-  const { changeSortingOnSelect, adsData } = useAdvertsContext();
-  const [selectedOption, setSelectedOption] = useState('Relevans');
+  const { changeSortingOnSelect, adsData, queries } = useAdvertsContext();
+  let selectValue: string;
+
+  const initialSort = queries.find((q) => q.query === 'sort=');
+  if (initialSort?.value === 'relevance') {
+    selectValue = 'Relevans';
+  } else if (initialSort?.value === 'applydate-desc') {
+    selectValue = 'Ansökningsdatum';
+  } else if (initialSort?.value === 'pubdate-desc') {
+    selectValue = 'Publiceringsdatum';
+  } else {
+    selectValue = 'Relevans';
+  }
+  const [selectedOption, setSelectedOption] = useState(selectValue);
 
   const handleSelectChange = (
     e: DigiFormSelectCustomEvent<HTMLDigiFormSelectElement>
